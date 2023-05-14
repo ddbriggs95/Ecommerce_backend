fb08f6c81828059b9b11f2134a1f91c4f9535000
ddbriggs95 <dowlingbriggs@gmail.com>
2023-05-14 18:31:43 -0500

    Made create new category post request and update category put reqest

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -32,12 +32,38 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
-  
+  Category.create({
+    category_name: req.body.category_name
+  })
+  .then((newCat) => {
+    res.json(newCat);
+  })
+  .catch((err) => {
+    res.json(err);
+  })
 });
+ 
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
-});
+    Category.update(
+      {
+        category_name: req.body.category_name
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      })
+      .then((updatedCat) => {
+        res.json(updatedCat);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.json(err);
+      })
+    }
+  )
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
